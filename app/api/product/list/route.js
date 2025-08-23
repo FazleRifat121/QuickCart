4daefa02ba7f5638acdd2ad0866b5f2429d55b2a
FazleRifat121 <fazlerifatofficial@gmail.com>
2025-08-23 16:46:30 +0600

    products showing from api

--- app/api/product/list/route.js
+++ app/api/product/list/route.js
@@ -1,19 +1,9 @@
 import connectDB from "@/config/db";
-import authSeller from "@/lib/authSeller";
 import Product from "@/models/product";
-import { getAuth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
 export async function GET(request) {
 	try {
-		const { userId } = getAuth(request);
-		const isSeller = authSeller(userId);
-		if (!isSeller) {
-			return NextResponse.json(
-				{ success: false, message: "Unauthorized" },
-				{ status: 403 }
-			);
-		}
 		await connectDB();
 		const products = await Product.find({});
 		return NextResponse.json({ success: true, products }, { status: 200 });
