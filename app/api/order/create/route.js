26358a02029a36ee48f4d56480f385b4f157bd57
FazleRifat121 <fazlerifatofficial@gmail.com>
2025-08-24 11:50:34 +0600

    fixed some issue

--- app/api/order/create/route.js
+++ app/api/order/create/route.js
@@ -9,26 +9,19 @@ export async function POST(request) {
 		const { userId } = getAuth(request);
 		const { address, items } = await request.json();
 
-		if (!address || !items || items.length === 0) {
+		if (!address || items.length === 0) {
 			return NextResponse.json(
-				{ success: false, message: "Invalid order data" },
+				{ success: false, message: "Invalid  data" },
 				{ status: 400 }
 			);
 		}
 
 		// ✅ calculate price properly
-		let amount = 0;
-		for (const item of items) {
+		const amount = await items.reduce(async (accP, item) => {
+			const acc = await accP;
 			const product = await Product.findById(item.product);
-			if (!product) {
-				return NextResponse.json(
-					{ success: false, message: "Product not found" },
-					{ status: 404 }
-				);
-			}
-			amount += product.offerPrice * item.quantity;
-		}
-		amount += Math.floor(amount * 0.02); // add 2% fee
+			return acc + product.offerPrice * item.quantity;
+		}, 0);
 
 		// ✅ send order event with slash
 		await inngest.send({
@@ -37,17 +30,15 @@ export async function POST(request) {
 				userId,
 				address,
 				items,
-				amount,
+				amount: amount + Math.floor(amount * 0.02),
 				date: Date.now(),
 			},
 		});
 
 		// clear user cart
 		const user = await User.findById(userId);
-		if (user) {
-			user.cart = {};
-			await user.save();
-		}
+		user.cartItems = [];
+		await user.save();
 
 		return NextResponse.json(
 			{ success: true, message: "Order Placed" },
