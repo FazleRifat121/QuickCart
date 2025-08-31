b8bc37487f23679de76281ee8ecfb8a3c9175302
FazleRifat121 <fazlerifatofficial@gmail.com>
2025-08-31 13:04:53 +0600

    popular product not gonna show more than 10

--- components/HomeProducts.jsx
+++ components/HomeProducts.jsx
@@ -5,11 +5,14 @@ import { useAppContext } from "@/context/AppContext";
 const HomeProducts = () => {
 	const { products, router } = useAppContext();
 
+	// Limit to 10 products
+	const displayedProducts = products.slice(0, 10);
+
 	return (
 		<div className="flex flex-col items-center pt-14">
 			<p className="text-2xl font-medium text-left w-full">Popular products</p>
 			<div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 flex-col items-center gap-6 mt-6 pb-14 w-full">
-				{products.map((product, index) => (
+				{displayedProducts.map((product, index) => (
 					<ProductCard key={index} product={product} />
 				))}
 			</div>
