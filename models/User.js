b93edda06daa95847ea91b9673a7412d768d3250
FazleRifat121 <fazlerifatofficial@gmail.com>
2025-09-01 14:26:32 +0600

    next gonna create public role can be change by admin

--- models/User.js
+++ models/User.js
@@ -1,15 +1,14 @@
+// models/User.js
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema(
-	{
-		_id: { type: String, required: true },
-		name: { type: String, required: true },
-		email: { type: String, required: true, unique: true },
-		imageUrl: { type: String, required: true },
-		cartItems: { type: Object, default: {} },
-	},
-	{ minimize: false }
-);
+const userSchema = new mongoose.Schema({
+	_id: { type: String, required: true },
+	name: { type: String, required: true },
+	email: { type: String, required: true, unique: true },
+	imageUrl: { type: String, required: true },
+	cartItems: { type: Object, default: {} },
+	publicRole: { type: String, enum: ["normal", "seller"], default: "normal" },
+});
 
 const User = mongoose.models.User || mongoose.model("User", userSchema);
 export default User;
