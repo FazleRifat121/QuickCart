e964a5cb6b29e68bb63029983b1aaa51d657cba1
FazleRifat121 <fazlerifatofficial@gmail.com>
2025-09-01 15:26:13 +0600

    now seller can make other account as seller

--- config/inngest.js
+++ config/inngest.js
@@ -7,25 +7,25 @@ import Order from "@/models/order";
 export const inngest = new Inngest({ id: "quickcart-next" });
 //Inngest create User
 export const syncUserCreation = inngest.createFunction(
-	{
-		id: "sync-user-from-clerk",
-	},
-	{
-		event: "clerk/user.created",
-	},
+	{ id: "sync-user-from-clerk" },
+	{ event: "clerk/user.created" },
 	async ({ event }) => {
 		const { id, first_name, last_name, email_addresses, image_url } =
 			event.data;
-		const userData = {
-			_id: id,
-			email: email_addresses[0].email_address,
-			name: first_name + " " + last_name,
-			imageUrl: image_url,
-		};
+
 		await connectDB();
-		await User.create(userData);
+
+		await User.create({
+			_id: id,
+			name: `${first_name} ${last_name}`,
+			email: email_addresses[0].email_address,
+			imageUrl: image_url,
+			cartItems: {},
+			publicRole: "Normal", // default role
+		});
 	}
 );
+
 // Inngest Function to update user data in database
 
 export const syncUserUpdatation = inngest.createFunction(
--- models/User.js
+++ models/User.js
@@ -1,13 +1,12 @@
-// models/User.js
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-	_id: { type: String, required: true },
+	_id: { type: String, required: true }, // Clerk user ID
 	name: { type: String, required: true },
 	email: { type: String, required: true, unique: true },
 	imageUrl: { type: String, required: true },
 	cartItems: { type: Object, default: {} },
-	publicRole: { type: String, enum: ["normal", "seller"], default: "normal" },
+	publicRole: { type: String, enum: ["Normal", "Seller"], default: "Normal" },
 });
 
 const User = mongoose.models.User || mongoose.model("User", userSchema);
