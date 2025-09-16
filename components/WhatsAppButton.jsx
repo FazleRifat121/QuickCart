2e24885683e5dff7761c19499a3168bccb1deded
FazleRifat121 <fazlerifatofficial@gmail.com>
2025-09-16 16:26:26 +0600

    whatsApp button ,go to top button color changed

--- components/WhatsAppButton.jsx
+++ components/WhatsAppButton.jsx
@@ -1,4 +1,5 @@
 "use client";
+
 import { useEffect, useState } from "react";
 import { IoLogoWhatsapp } from "react-icons/io5";
 import Link from "next/link";
@@ -7,27 +8,26 @@ export default function WhatsAppButton() {
 	const [mounted, setMounted] = useState(false);
 	const [show, setShow] = useState(false);
 
-	// Only run on client
 	useEffect(() => {
 		setMounted(true); // mark as mounted
-		const timer = setTimeout(() => setShow(true), 3000);
+		const timer = setTimeout(() => setShow(true), 3000); // delay
 		return () => clearTimeout(timer);
 	}, []);
 
-	if (!mounted) return null; // render nothing on server
+	if (!mounted) return null; // prevent SSR mismatch
 
 	return (
 		<span
 			className={`fixed top-48 ${
 				show ? "right-0" : "-right-[200px]"
-			} bg-orange-700 text-white rounded-s-full p-2 w-[60px] text-2xl cursor-pointer transition-all duration-300 z-50`}
+			} bg-orange-600 hover:pr-8 text-white p-2 text-2xl cursor-pointer transition-all duration-300 rounded-l-md z-50`}
 		>
 			<Link
-				href="https://wa.me/8801327184165"
+				href="https://wa.me/8801722080196"
 				target="_blank"
 				rel="noopener noreferrer"
 			>
-				<IoLogoWhatsapp className="text-3xl ml-3" />
+				<IoLogoWhatsapp className="text-3xl ml-1" />
 			</Link>
 		</span>
 	);
